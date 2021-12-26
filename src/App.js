c7c2b963773646568f2f22c3ddfa2450b492763e
David Rawson <dm.rawson@gmail.com>
2021-12-26 22:50:54 +0000

    Initial commit. Cards displayed and pagination working.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
 
-function App() {
+// Components
+import Card from "./components/Card/Card";
+import Filter from "./components/Filter/Filter";
+import Navbar from "./components/Navbar/Navbar";
+import Pagination from "./components/Pagination/Pagination";
+import Search from "./components/Search/Search";
+
+const App = () => {
+  const [fetchedData, setFetchedData] = useState([]);
+  const { info, results } = fetchedData;
+  const [pageNumber, setPageNumber] = useState(1);
+  const [search, setSearch] = useState("");
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch(api).then((res) => res.json());
+      setFetchedData(data);
+    };
+
+    fetchData();
+  }, [api]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-center mb-3">Characters</h1>
+      <Search setSearch={setSearch} setPageNumber={setPageNumber} />
+      <div className="container">
+        <div className="row">
+          Filter component to go here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        setPageNumber={setPageNumber}
+      />
     </div>
   );
-}
+};
 
 export default App;
